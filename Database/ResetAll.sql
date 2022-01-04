dbf1973322c631706e30803a703574e6689ae39b
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-01-04 15:08:36 +0700

    Thêm SQL Reset / Delete all data

---
+++ Database/ResetAll.sql
@@ -0,0 +1,11 @@
+USE QuanLyQuanTraSua
+DROP TABLE NhanVien
+DROP TABLE TaiKhoan
+DROP TABLE LichLam
+DROP TABLE KhoHang
+DROP TABLE VatLieu
+DROP TABLE SanPham
+DROP TABLE Menu
+DROP TABLE HoaDon
+
+--DROP TABLE BaoCao => Make it as a View
