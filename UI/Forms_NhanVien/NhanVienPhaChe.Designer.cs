2d27c2f4f93ae4cd6225377c0cd30f3d1c73d62c
Keios Starqua <khoi0941@gmail.com>
2022-03-24 22:18:40 +0700

    (#55)  NhanVienPhaChe

--- UI/Forms_NhanVien/NhanVienPhaChe.Designer.cs
+++ UI/Forms_NhanVien/NhanVienPhaChe.Designer.cs
@@ -57,13 +57,13 @@ namespace ChinChin.Forms_NhanVien
             // 
             this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMinimize.BackColor = System.Drawing.Color.White;
-            this.btnMinimize.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnMinimize.IconChar = FontAwesome.Sharp.IconChar.MinusCircle;
-            this.btnMinimize.IconColor = System.Drawing.SystemColors.ControlText;
+            this.btnMinimize.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnMinimize.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnMinimize.IconSize = 46;
             this.btnMinimize.Location = new System.Drawing.Point(1272, 2);
-            this.btnMinimize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(2);
             this.btnMinimize.Name = "btnMinimize";
             this.btnMinimize.Size = new System.Drawing.Size(46, 50);
             this.btnMinimize.TabIndex = 4;
@@ -74,13 +74,13 @@ namespace ChinChin.Forms_NhanVien
             // 
             this.btnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMaximize.BackColor = System.Drawing.Color.White;
-            this.btnMaximize.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMaximize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnMaximize.IconChar = FontAwesome.Sharp.IconChar.ExpandArrowsAlt;
-            this.btnMaximize.IconColor = System.Drawing.SystemColors.ControlText;
+            this.btnMaximize.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnMaximize.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnMaximize.IconSize = 49;
             this.btnMaximize.Location = new System.Drawing.Point(1323, 2);
-            this.btnMaximize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMaximize.Margin = new System.Windows.Forms.Padding(2);
             this.btnMaximize.Name = "btnMaximize";
             this.btnMaximize.Size = new System.Drawing.Size(50, 49);
             this.btnMaximize.TabIndex = 3;
@@ -91,13 +91,13 @@ namespace ChinChin.Forms_NhanVien
             // 
             this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnExit.BackColor = System.Drawing.Color.White;
-            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnExit.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
-            this.btnExit.IconColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.btnExit.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnExit.IconSize = 47;
             this.btnExit.Location = new System.Drawing.Point(1378, 2);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(47, 50);
             this.btnExit.TabIndex = 2;
@@ -107,13 +107,13 @@ namespace ChinChin.Forms_NhanVien
             // iconCurrentChildForm
             // 
             this.iconCurrentChildForm.BackColor = System.Drawing.Color.Transparent;
-            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.Black;
+            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.iconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.Home;
-            this.iconCurrentChildForm.IconColor = System.Drawing.Color.Black;
+            this.iconCurrentChildForm.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.iconCurrentChildForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconCurrentChildForm.IconSize = 46;
             this.iconCurrentChildForm.Location = new System.Drawing.Point(0, 9);
-            this.iconCurrentChildForm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.iconCurrentChildForm.Margin = new System.Windows.Forms.Padding(2);
             this.iconCurrentChildForm.Name = "iconCurrentChildForm";
             this.iconCurrentChildForm.Size = new System.Drawing.Size(46, 50);
             this.iconCurrentChildForm.TabIndex = 0;
@@ -129,7 +129,7 @@ namespace ChinChin.Forms_NhanVien
             this.panelTittleBar.Controls.Add(this.labelTittleChildForm);
             this.panelTittleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTittleBar.Location = new System.Drawing.Point(0, 0);
-            this.panelTittleBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelTittleBar.Margin = new System.Windows.Forms.Padding(2);
             this.panelTittleBar.Name = "panelTittleBar";
             this.panelTittleBar.Size = new System.Drawing.Size(1426, 61);
             this.panelTittleBar.TabIndex = 7;
@@ -157,9 +157,9 @@ namespace ChinChin.Forms_NhanVien
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.ForeColor = System.Drawing.Color.Coral;
             this.panelMenu.Location = new System.Drawing.Point(0, 61);
-            this.panelMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(2);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(376, 778);
+            this.panelMenu.Size = new System.Drawing.Size(376, 497);
             this.panelMenu.TabIndex = 10;
             // 
             // TaiKhoanBTN
@@ -170,11 +170,11 @@ namespace ChinChin.Forms_NhanVien
             this.TaiKhoanBTN.Font = new System.Drawing.Font("Cascadia Code", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TaiKhoanBTN.ForeColor = System.Drawing.SystemColors.ControlText;
             this.TaiKhoanBTN.IconChar = FontAwesome.Sharp.IconChar.Boxes;
-            this.TaiKhoanBTN.IconColor = System.Drawing.Color.Black;
+            this.TaiKhoanBTN.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.TaiKhoanBTN.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.TaiKhoanBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.TaiKhoanBTN.Location = new System.Drawing.Point(0, 224);
-            this.TaiKhoanBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TaiKhoanBTN.Margin = new System.Windows.Forms.Padding(2);
             this.TaiKhoanBTN.Name = "TaiKhoanBTN";
             this.TaiKhoanBTN.Padding = new System.Windows.Forms.Padding(8, 0, 15, 0);
             this.TaiKhoanBTN.Size = new System.Drawing.Size(374, 102);
@@ -192,11 +192,11 @@ namespace ChinChin.Forms_NhanVien
             this.KhoQuayBTN.Font = new System.Drawing.Font("Cascadia Code", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.KhoQuayBTN.ForeColor = System.Drawing.SystemColors.ControlText;
             this.KhoQuayBTN.IconChar = FontAwesome.Sharp.IconChar.Dashcube;
-            this.KhoQuayBTN.IconColor = System.Drawing.Color.Black;
+            this.KhoQuayBTN.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.KhoQuayBTN.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.KhoQuayBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.KhoQuayBTN.Location = new System.Drawing.Point(0, 122);
-            this.KhoQuayBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.KhoQuayBTN.Margin = new System.Windows.Forms.Padding(2);
             this.KhoQuayBTN.Name = "KhoQuayBTN";
             this.KhoQuayBTN.Padding = new System.Windows.Forms.Padding(8, 0, 15, 0);
             this.KhoQuayBTN.Size = new System.Drawing.Size(374, 102);
@@ -215,15 +215,15 @@ namespace ChinChin.Forms_NhanVien
             this.panelLogo.Controls.Add(this.btnHome);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
-            this.panelLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(2);
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(374, 122);
             this.panelLogo.TabIndex = 0;
             // 
             // SignOutButton
             // 
-            this.SignOutButton.BackColor = System.Drawing.Color.Black;
-            this.SignOutButton.BackgroundColor = System.Drawing.Color.Black;
+            this.SignOutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
+            this.SignOutButton.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
             this.SignOutButton.BorderColor = System.Drawing.Color.Black;
             this.SignOutButton.BorderRadius = 0;
             this.SignOutButton.BorderSize = 0;
@@ -232,7 +232,7 @@ namespace ChinChin.Forms_NhanVien
             this.SignOutButton.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SignOutButton.ForeColor = System.Drawing.Color.White;
             this.SignOutButton.Location = new System.Drawing.Point(182, 7);
-            this.SignOutButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SignOutButton.Margin = new System.Windows.Forms.Padding(2);
             this.SignOutButton.Name = "SignOutButton";
             this.SignOutButton.Size = new System.Drawing.Size(188, 41);
             this.SignOutButton.TabIndex = 11;
@@ -272,21 +272,21 @@ namespace ChinChin.Forms_NhanVien
             this.panelDesktop.BackgroundImage = global::ChinChin.Properties.Resources.background_1920x1920;
             this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelDesktop.Location = new System.Drawing.Point(376, 61);
-            this.panelDesktop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelDesktop.Margin = new System.Windows.Forms.Padding(2);
             this.panelDesktop.Name = "panelDesktop";
-            this.panelDesktop.Size = new System.Drawing.Size(1050, 778);
+            this.panelDesktop.Size = new System.Drawing.Size(1050, 497);
             this.panelDesktop.TabIndex = 11;
             // 
             // NhanVienPhaChe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1426, 839);
+            this.ClientSize = new System.Drawing.Size(1426, 558);
             this.Controls.Add(this.panelDesktop);
             this.Controls.Add(this.panelMenu);
             this.Controls.Add(this.panelTittleBar);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "NhanVienPhaChe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NhanVienPhaChe";
