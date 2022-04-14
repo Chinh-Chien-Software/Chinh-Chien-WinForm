5c70070f9beb2a8eea237b77475f81b89cbbdca8
Keios Starqua <khoi0941@gmail.com>
2022-04-14 11:18:59 +0700

    Update Database
    * Refactor in DataProvider
    	- cn to conn
    	- cnStr to connStr

--- Database/DataProvider.cs
+++ Database/DataProvider.cs
@@ -13,25 +13,30 @@ namespace ChinChin.Database
     internal class DataProvider
     {
         // cnStr chứa thông tin về tên Server, Database..
-        static string cnStr = @"Data Source=DESKTOP-AN8O8G6\TQK;Initial Catalog=QuanLyQuanTraSua;Integrated Security=True";
+        static SqlConnection conn; // Create a connection
+        static string connStr = @"Data Source=DESKTOP-AN8O8G6\TQK;Initial Catalog=QuanLyQuanTraSua;Integrated Security=True";
 
-        static SqlConnection cn; // Create a connection
-        
         static public DataTable LoadDatabase(string sql)
         {
             DataTable dt = new DataTable(); //Khởi tạo một datatable để lưu dữ liệu
-            cn = new SqlConnection(cnStr); //Khởi tạo 1 kết nối
-            SqlCommand cm = new SqlCommand(sql, cn); //Tạo các lệnh
+            conn = new SqlConnection(connStr); //Khởi tạo 1 kết nối
+            SqlCommand cm = new SqlCommand(sql, conn); //Tạo các lệnh
             SqlDataAdapter da = new SqlDataAdapter(cm); //Thực thi những lệnh được truyền từ SqlCommand
             da.Fill(dt); //Đổ dữ liệu từ DataAdapter vào Datatable
             return dt;
         }
+
+        static public Create_Check_Database ()
+        {
+
+        }
+
         // Tại sao lại tạo DataSet trong khi chỉ toàn dùng DataTable
         static public DataSet LoadCSDLDTS(string sql)
         {
             DataSet dt = new DataSet();//Khởi tạo một datatable để lưu dữ liệu
-            cn = new SqlConnection(cnStr);//Khởi tạo 1 kết nối
-            SqlCommand cm = new SqlCommand(sql, cn);//Tạo các lệnh
+            conn = new SqlConnection(connStr);//Khởi tạo 1 kết nối
+            SqlCommand cm = new SqlCommand(sql, conn);//Tạo các lệnh
             SqlDataAdapter da = new SqlDataAdapter(cm);//Thực thi những lệnh được truyền từ SqlCommand
             da.Fill(dt);//Đổ dữ liệu từ DataAdapter vào Datatable
             return dt;
