d7fd327115c5881baa63e82f470c1ec520a94423
Keios Starqua <khoi0941@gmail.com>
2022-04-21 17:15:22 +0700

    Tạo lớp TaiKhoan,
    thêm các thuộc tính cho lớp

---
+++ Classes/TaiKhoan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinChin.Classes
+{
+    internal class TaiKhoan
+    {
+        string TenTaiKhoan;
+        string LoaiTaiKhoan;
+        string MaNhanVien;
+        int UIMode;
+    }
+}
