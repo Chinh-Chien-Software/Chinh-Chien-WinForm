26913d6c9bf46cf746a383d1b5906e240ec168d8
Tur PahnLo <logan.hphan@gmail.com>
2022-04-26 09:53:17 +0700

    Hiện thông tin tài khoản: mã, tên, giới tính nv, ngày vào làm, luong, chức vụ, và thông tin liên hệ.

--- UI/ThongTinTaiKhoan.cs
+++ UI/ThongTinTaiKhoan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChinChin.Database;
 
 namespace ChinChin.UI
 {
@@ -16,10 +17,27 @@ namespace ChinChin.UI
         {
             InitializeComponent();
         }
-
+        public string TenTaiKhoan;
+        public ThongTinTaiKhoan(string TenTaiKhoan) : this()
+        {
+            // Viết hàm khởi tạo này để mang theo dữ liệu khi chạy Form MainUI
+            this.TenTaiKhoan = TenTaiKhoan;
+        }
         private void ThongTinTaiKhoan_Load(object sender, EventArgs e)
         {
-            
+            string sqlcode = "SELECT nv.MaNhanVien, nv.TenNhanVien, nv.GioiTinh, nv.LoaiNhanVien, nv.NgayVaoLam, nv.LuongTrenGio, nv.NgaySinh, nv.DiaChi, nv.SoDienThoai FROM TaiKhoan tk, NhanVien nv WHERE tk.TenTaiKhoan = '"+TenTaiKhoan+"' and tk.MaNhanVien = nv.MaNhanVien";
+            DataTable TaiKhoan = new DataTable();
+            TaiKhoan = DataProvider.LoadDatabase(sqlcode);
+
+            tbMaNhanVien.Text = TaiKhoan.Rows[0][0].ToString();
+            tbTenNhanVien.Text = TaiKhoan.Rows[0][1].ToString();
+            tbGioiTinh.Text = TaiKhoan.Rows[0][2].ToString();
+            tbChucVu.Text = TaiKhoan.Rows[0][3].ToString();
+            tbNgayVaoLam.Text = TaiKhoan.Rows[0][4].ToString();
+            tbTienLuong.Text = TaiKhoan.Rows[0][5].ToString();
+            tbNgaySinh.Text = TaiKhoan.Rows[0][6].ToString();
+            tbDiaChi.Text = TaiKhoan.Rows[0][7].ToString();
+            tbSDT.Text = TaiKhoan.Rows[0][8].ToString();
         }
 
         private void btDoiMatKhau_Click(object sender, EventArgs e)
