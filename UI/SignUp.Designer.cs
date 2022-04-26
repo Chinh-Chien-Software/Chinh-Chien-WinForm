2566cb2587419dbcc31eb767eeb5ddcb516489d9
Keios Starqua <khoi0941@gmail.com>
2022-04-26 10:06:17 +0700

    update name of element of SignUp

--- UI/SignUp.Designer.cs
+++ UI/SignUp.Designer.cs
@@ -28,24 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTenTaiKhoan = new System.Windows.Forms.Label();
+            this.txbTenTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTenTaiKhoan
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(308, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblTenTaiKhoan.AutoSize = true;
+            this.lblTenTaiKhoan.Location = new System.Drawing.Point(99, 54);
+            this.lblTenTaiKhoan.Name = "lblTenTaiKhoan";
+            this.lblTenTaiKhoan.Size = new System.Drawing.Size(78, 13);
+            this.lblTenTaiKhoan.TabIndex = 0;
+            this.lblTenTaiKhoan.Text = "Tên Tài Khoản";
+            this.lblTenTaiKhoan.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txbTenTaiKhoan
+            // 
+            this.txbTenTaiKhoan.Location = new System.Drawing.Point(193, 47);
+            this.txbTenTaiKhoan.Name = "txbTenTaiKhoan";
+            this.txbTenTaiKhoan.Size = new System.Drawing.Size(100, 20);
+            this.txbTenTaiKhoan.TabIndex = 1;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Location = new System.Drawing.Point(193, 82);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(100, 20);
+            this.txbEmail.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(99, 89);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
             // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txbTenTaiKhoan);
+            this.Controls.Add(this.lblTenTaiKhoan);
             this.Name = "SignUp";
             this.Text = "SignUp";
             this.ResumeLayout(false);
@@ -55,6 +85,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTenTaiKhoan;
+        private System.Windows.Forms.TextBox txbTenTaiKhoan;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label lblEmail;
     }
 }
--- UI/SignUp.cs
+++ UI/SignUp.cs
@@ -16,5 +16,10 @@ namespace ChinChin.UI
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
