4e1dc8d831acf4186d6edaa7dad9a195c0337a2f
Keios Starqua <khoi0941@gmail.com>
2022-05-01 21:23:56 +0700

    Cập nhật Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ChinChin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LogIn());
+            Application.Run(new ChinChin.UI.LogIn());
         }
     }
 }
