c07320b3e7755f1072c0ad018634edc864730f26
Keios Starqua <khoi0941@gmail.com>
2022-05-09 22:09:10 +0700

    Tạo lớp theo mô hình 3 lớp
    * TaiKhoanDAO.cs
    * HoaDonVO.cs

---
+++ VO/HoaDonVO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinChin.Classes
+{
+    internal class HoaDonVO
+    {
+        private string _maHD { get; set; }
+        private string _maNV { get; set; }
+        private DateTime _thoiGianGiaoDich { get; set; }
+    }
+}
