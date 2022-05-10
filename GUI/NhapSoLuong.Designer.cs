cd09d6c3345a5ced6c793a4ee105b28f259c0289
Keios Starqua <khoi0941@gmail.com>
2022-05-10 17:29:25 +0700

    Xong Form NhapSoLuong
    với thuộc tính SoLuong dùng đề return dữ liệu

---
+++ GUI/NhapSoLuong.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ChinChin.GUI
+{
+    partial class NhapSoLuong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.tbcSL = new ChinChin.Custom.TextBox_Custom();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserName.Font = new System.Drawing.Font("Inter", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(11, 9);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(122, 33);
+            this.lblUserName.TabIndex = 10;
+            this.lblUserName.Text = "Số lượng";
+            this.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbcSL
+            // 
+            this.tbcSL.BackColor = System.Drawing.Color.White;
+            this.tbcSL.ForeColor = System.Drawing.Color.White;
+            this.tbcSL.Location = new System.Drawing.Point(17, 46);
+            this.tbcSL.Margin = new System.Windows.Forms.Padding(4);
+            this.tbcSL.Name = "tbcSL";
+            this.tbcSL.PasswordChar = '\0';
+            this.tbcSL.Size = new System.Drawing.Size(362, 40);
+            this.tbcSL.TabIndex = 11;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
+            this.btnOK.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(108)))), ((int)(((byte)(247)))));
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Inter", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(386, 46);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 40);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "Thành Tiền";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // NhapSoLuong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 97);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.tbcSL);
+            this.Name = "NhapSoLuong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NhapSoLuong";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private Custom.TextBox_Custom tbcSL;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ GUI/NhapSoLuong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChinChin.GUI
+{
+    public partial class NhapSoLuong : Form
+    {
+        public NhapSoLuong()
+        {
+            InitializeComponent();
+            this.ActiveControl = tbcSL;
+        }
+        
+        public string SoLuong
+        {
+            get;
+            set;
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            SoLuong = tbcSL.Text;
+            this.Close();
+        }
+    }
+}
