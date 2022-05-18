1e17c428e7eec015f990bef0bcc4fbcf818f4a25
Keios Starqua <khoi0941@gmail.com>
2022-05-18 11:37:24 +0700

    Tạo ThemCapnhatQuan

--- GUI/formThem/ThemCapnhatQuan.Designer.cs
+++ GUI/formThem/ThemCapnhatQuan.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ThemCapnhatQuan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(751, 385);
+            this.Name = "ThemCapnhatQuan";
             this.Text = "ThemCapnhatQuan";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
