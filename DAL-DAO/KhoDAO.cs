eb9994b03e2e1bd517ea924ca5e5d85aec038548
Keios Starqua <khoi0941@gmail.com>
2022-05-22 15:17:17 +0700

    Viết method LayKho
    * và dùng nó để lấy dữ liệu tùy theo quán

---
+++ DAL-DAO/KhoDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ChinChin.DAL_DAO
+{
+    internal class KhoDAO
+    {
+        public DataTable LayKho(string MaQuan)
+        {
+            string sql = "select * from Kho where MaQuan = '"+MaQuan+"'";
+            DataTable dt = DataProvider.ReturnDataTable(sql);
+            return dt;
+        }
+    }
+}
