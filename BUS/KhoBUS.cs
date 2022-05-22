717ed7def88b4c27c533847e99617dd520fa4123
Keios Starqua <khoi0941@gmail.com>
2022-05-22 20:02:15 +0700

    Viết method LayTenKhoBangMaKho cho KhoBUS

--- BUS/KhoBUS.cs
+++ BUS/KhoBUS.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChinChin.DAL_DAO;
+using System.Data;
 
 namespace ChinChin.BUS
 {
     internal class KhoBUS
     {
+        KhoDAO khoDAO = new KhoDAO();
+        
+        public string LayTenKhoBangMaKho(string MaKho, string MaQuan)
+        {
+            DataTable dt = khoDAO.LayKhoBangMaKho(MaKho, MaQuan);
+            return dt.Columns[0].ToString();   
+        }
     }
 }
