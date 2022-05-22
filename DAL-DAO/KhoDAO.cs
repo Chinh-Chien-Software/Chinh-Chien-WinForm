7f0c361ab664d9da748e121ff106c94ebbd7075b
Keios Starqua <khoi0941@gmail.com>
2022-05-22 20:02:50 +0700

    VIết method LayKhoBangMaKho cho KhoDAO

--- DAL-DAO/KhoDAO.cs
+++ DAL-DAO/KhoDAO.cs
@@ -9,11 +9,17 @@ namespace ChinChin.DAL_DAO
 {
     internal class KhoDAO
     {
-        public DataTable LayKho(string MaQuan)
+        public DataTable LayDanhSachKho(string MaQuan)
         {
             string sql = "select * from Kho where MaQuan = '"+MaQuan+"'";
             DataTable dt = DataProvider.ReturnDataTable(sql);
             return dt;
+        }
+
+        public DataTable LayKhoBangMaKho (string MaKho, string MaQuan)
+        {
+            return DataProvider.ReturnDataTable("select TenKho from Kho where MaQuan = '" + MaQuan +
+                "' and MaKho = '" + MaKho + "'");
         }
     }
 }
