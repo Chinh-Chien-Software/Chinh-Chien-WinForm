d025841206d33d57083a50187419f488060fc351
Keios Starqua <khoi0941@gmail.com>
2022-05-24 15:33:43 +0700

    Rename and Moved BaoCaoOriginal to folder BaoCao
    * clean BaoCao form

--- GUI/BaoCao.Designer.cs
+++ GUI/BaoCao.Designer.cs
@@ -29,27 +29,26 @@ namespace ChinChin.GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.SoLuongVatLieu1 = new ChinChin.BaoCao.SoLuongVatLieu();
+            this.cRV = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.SuspendLayout();
             // 
-            // crystalReportViewer1
+            // cRV
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
-            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
-            this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
-            this.crystalReportViewer1.TabIndex = 0;
+            this.cRV.ActiveViewIndex = -1;
+            this.cRV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cRV.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cRV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cRV.Location = new System.Drawing.Point(0, 0);
+            this.cRV.Name = "cRV";
+            this.cRV.Size = new System.Drawing.Size(800, 450);
+            this.cRV.TabIndex = 0;
             // 
             // BaoCao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.cRV);
             this.Name = "BaoCao";
             this.Text = "BaoCao";
             this.ResumeLayout(false);
@@ -58,7 +57,6 @@ namespace ChinChin.GUI
 
         #endregion
 
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
-        private ChinChin.BaoCao.SoLuongVatLieu SoLuongVatLieu1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer cRV;
     }
 }
