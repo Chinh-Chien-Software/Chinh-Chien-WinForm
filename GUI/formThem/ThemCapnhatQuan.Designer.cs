c0f36e46a7d2cd0172b3de68ca99d2187ac92e51
Keios Starqua <khoi0941@gmail.com>
2022-05-29 15:38:07 +0700

    Thiết kế xong UI

--- GUI/formThem/ThemCapnhatQuan.Designer.cs
+++ GUI/formThem/ThemCapnhatQuan.Designer.cs
@@ -28,20 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlMoveAndTitle = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnExit = new FontAwesome.Sharp.IconPictureBox();
+            this.lblMaQuan = new System.Windows.Forms.Label();
+            this.tbcMaQuan = new ChinChin.Custom.TextBox_Custom();
+            this.lblTenQuan = new System.Windows.Forms.Label();
+            this.tbcTenQuan = new ChinChin.Custom.TextBox_Custom();
+            this.pnlMoveAndTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pnlMoveAndTitle
+            // 
+            this.pnlMoveAndTitle.Controls.Add(this.btnExit);
+            this.pnlMoveAndTitle.Controls.Add(this.lblTitle);
+            this.pnlMoveAndTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMoveAndTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlMoveAndTitle.Name = "pnlMoveAndTitle";
+            this.pnlMoveAndTitle.Size = new System.Drawing.Size(416, 55);
+            this.pnlMoveAndTitle.TabIndex = 32;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblTitle.Font = new System.Drawing.Font("Cascadia Code", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(489, 55);
+            this.lblTitle.TabIndex = 30;
+            this.lblTitle.Text = "Thêm Cập nhật Quán";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
+            this.btnExit.IconColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnExit.IconSize = 46;
+            this.btnExit.Location = new System.Drawing.Point(368, 2);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(46, 48);
+            this.btnExit.TabIndex = 29;
+            this.btnExit.TabStop = false;
+            // 
+            // lblMaQuan
+            // 
+            this.lblMaQuan.AutoSize = true;
+            this.lblMaQuan.BackColor = System.Drawing.Color.Transparent;
+            this.lblMaQuan.Font = new System.Drawing.Font("Inter", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaQuan.Location = new System.Drawing.Point(11, 71);
+            this.lblMaQuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMaQuan.Name = "lblMaQuan";
+            this.lblMaQuan.Size = new System.Drawing.Size(122, 33);
+            this.lblMaQuan.TabIndex = 33;
+            this.lblMaQuan.Text = "Mã quán";
+            this.lblMaQuan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbcMaQuan
+            // 
+            this.tbcMaQuan.BackColor = System.Drawing.Color.White;
+            this.tbcMaQuan.ForeColor = System.Drawing.Color.White;
+            this.tbcMaQuan.Location = new System.Drawing.Point(17, 108);
+            this.tbcMaQuan.Margin = new System.Windows.Forms.Padding(4);
+            this.tbcMaQuan.Name = "tbcMaQuan";
+            this.tbcMaQuan.PasswordChar = '\0';
+            this.tbcMaQuan.Size = new System.Drawing.Size(362, 40);
+            this.tbcMaQuan.TabIndex = 34;
+            // 
+            // lblTenQuan
+            // 
+            this.lblTenQuan.AutoSize = true;
+            this.lblTenQuan.BackColor = System.Drawing.Color.Transparent;
+            this.lblTenQuan.Font = new System.Drawing.Font("Inter", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenQuan.Location = new System.Drawing.Point(11, 152);
+            this.lblTenQuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTenQuan.Name = "lblTenQuan";
+            this.lblTenQuan.Size = new System.Drawing.Size(159, 33);
+            this.lblTenQuan.TabIndex = 35;
+            this.lblTenQuan.Text = "Tên hiển thị";
+            this.lblTenQuan.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbcTenQuan
+            // 
+            this.tbcTenQuan.ForeColor = System.Drawing.Color.White;
+            this.tbcTenQuan.Location = new System.Drawing.Point(17, 191);
+            this.tbcTenQuan.Margin = new System.Windows.Forms.Padding(6);
+            this.tbcTenQuan.Name = "tbcTenQuan";
+            this.tbcTenQuan.PasswordChar = '\0';
+            this.tbcTenQuan.Size = new System.Drawing.Size(362, 40);
+            this.tbcTenQuan.TabIndex = 36;
             // 
             // ThemCapnhatQuan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(751, 385);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(416, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblMaQuan);
+            this.Controls.Add(this.tbcMaQuan);
+            this.Controls.Add(this.lblTenQuan);
+            this.Controls.Add(this.tbcTenQuan);
+            this.Controls.Add(this.pnlMoveAndTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "ThemCapnhatQuan";
-            this.Text = "ThemCapnhatQuan";
+            this.pnlMoveAndTitle.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlMoveAndTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private FontAwesome.Sharp.IconPictureBox btnExit;
+        private System.Windows.Forms.Label lblMaQuan;
+        private Custom.TextBox_Custom tbcMaQuan;
+        private System.Windows.Forms.Label lblTenQuan;
+        private Custom.TextBox_Custom tbcTenQuan;
     }
 }
