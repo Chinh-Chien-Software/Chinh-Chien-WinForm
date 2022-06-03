f3fd83040113c1106be6e5f4ec58e4f17279da08
Tạ Quang Khôi <khoi0941@gmail.com>
2022-06-03 10:23:15 +0700

    Thêm class ThongBao

---
+++ GUI/ThongBao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChinhChien.GUI
+{
+    internal class ThongBao
+    {
+
+        public void Hien(Label lb, string message)
+        {
+            lb.Visible = true;
+            lb.Text = message;
+        }
+    }
+}
